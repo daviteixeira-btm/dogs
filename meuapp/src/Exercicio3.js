45e590715d0ac1f472fd73e0f1854a2ba788f31f
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-04-14 21:06:41 -0300

    Finalização React para Iniciantes

---
+++ meuapp/src/Exercicio3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exercicio3 = () => {
+    return (
+        <section>
+            
+        </section>
+    );
+}
--- meuapp/src/index.js
+++ meuapp/src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import Exercicio1 from './Exercicio1';
+import Exercicio2 from './Exercicio2';
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
+
+const {pathname} = window.location;
+
+let Pagina;
+
+if(pathname === '/produtos'){
+  Pagina = Produtos
+} else {
+  Pagina = Home
+}
+
 ReactDOM.render(
+
   <React.StrictMode>
+    <Header />
+    <Pagina />
+
+
     <App />
+    <Exercicio1 />
+    <Exercicio2 />
   </React.StrictMode>,
   document.getElementById('root'),
 );
